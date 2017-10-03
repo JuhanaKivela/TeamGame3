f192f6658425807101b43811a870640788002407
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2017-10-03 13:37:16 +0300

    pö

--- TeamGame2D/Assets/Player/PlayerScript.cs
+++ TeamGame2D/Assets/Player/PlayerScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    static Animator anim;
+    public float speed;
+
 	// Use this for initialization
 	void Start () {
-		//Häksätään kaikki pastat.
+
+        anim = GetComponent<Animator>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
+        }
+    }
 }
