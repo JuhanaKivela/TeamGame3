b4d5970464d9f68d4184a7a38f653cdfd3acc67b
JuhanaKivela <kivelajuhana@gmail.com>
2017-10-04 09:34:16 +0300

    atteonautistieikäsejaksaodottaakunminäkirjoitan pastaa

---
+++ TeamGame2D/Assets/Scenes/Map1/Land1.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Land1" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="WaterTerrain" tile="0"/>
+  <terrain name="DirtTerrain" tile="60"/>
+  <terrain name="StonePath" tile="1222"/>
+ </terraintypes>
+ <tile id="2" terrain=",,,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain=",,0,0">
+  <objectgroup draworder="index">
+   <object id="17" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain=",,0,">
+  <objectgroup draworder="index">
+   <object id="12" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="57" terrain="0,0,0,"/>
+ <tile id="58" terrain="0,0,,0"/>
+ <tile id="59" terrain=",0,,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60" terrain="0,0,0,0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61" terrain="0,,0,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="114" terrain="0,,0,0"/>
+ <tile id="115" terrain=",0,0,0"/>
+ <tile id="116" terrain=",0,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117" terrain="0,0,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118" terrain="0,,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="518" terrain="1,1,1,"/>
+ <tile id="519" terrain="1,1,,1"/>
+ <tile id="520" terrain=",,,1"/>
+ <tile id="521" terrain=",,1,1"/>
+ <tile id="522" terrain=",,1,"/>
+ <tile id="575" terrain="1,,1,1"/>
+ <tile id="576" terrain=",1,1,1"/>
+ <tile id="577" terrain=",1,,1"/>
+ <tile id="578" terrain="1,1,1,1"/>
+ <tile id="579" terrain="1,,1,"/>
+ <tile id="597">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="634" terrain=",1,,"/>
+ <tile id="635" terrain="1,1,,"/>
+ <tile id="636" terrain="1,,,"/>
+ <tile id="640">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="641">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="642">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="644">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="645">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="650">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="860" terrain="2,2,2,"/>
+ <tile id="861" terrain="2,2,,2"/>
+ <tile id="862" terrain=",,,2"/>
+ <tile id="863" terrain=",,2,2"/>
+ <tile id="864" terrain=",,2,"/>
+ <tile id="917" terrain="2,,2,2"/>
+ <tile id="918" terrain=",2,2,2"/>
+ <tile id="919" terrain=",2,,2"/>
+ <tile id="920" terrain="2,2,2,2"/>
+ <tile id="921" terrain="2,,2,"/>
+ <tile id="976" terrain=",2,,"/>
+ <tile id="977" terrain="2,2,,"/>
+ <tile id="978" terrain="2,,,"/>
+</tileset>
