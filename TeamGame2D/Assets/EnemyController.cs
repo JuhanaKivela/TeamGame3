8bc1c8fbda29816aa5207b86301c79b52fcdd4a6
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2017-10-04 14:05:04 +0300

    lll

---
+++ TeamGame2D/Assets/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform Player;
+    int moveSpeed = 15;
+    int maxDistance = 20;
+    int minDist = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= minDist)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= maxDistance)
+            {
+                
+            }
+        }
+	}
+}
