776d7d74e52143290b82646f971ef4ef8a722a6a
JuhanaKivela <kivelajuhana@gmail.com>
2017-10-06 10:05:16 +0300

    Character attack + code update

--- TeamGame2D/Assets/Player/PlayerScript.cs
+++ TeamGame2D/Assets/Player/PlayerScript.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     private Animator myAnimator;
-    public float speed;
+    public float speed = 30;
+    private bool attack;
 
 	// Use this for initialization
 	void Start () {
-
+        // Gets Animator component. "myAnimator" can be called later on
         myAnimator = GetComponent<Animator>();
 
-
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        
+        // Puts Horizontal and vertical movement to a variables. Shortents the code. 
+        // Variable can be called instead of Input.GetAxisRaw("");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float walkSpeed = 0;
 
         // Movement
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        if (moveHorizontal > 0.5f || moveHorizontal < -0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
         }
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        if (moveVertical > 0.5f || moveVertical < -0.5f)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
         }
         //End movement
 
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-        float walkSpeed = 0;
+        if (Input.GetAxisRaw("Fire1") > 0.5f && moveHorizontal == 0 && moveVertical == 0)
+        {
+            attack = true;
+        }
+        else
+        {
+            attack = false;
+        }
+        myAnimator.SetBool("attack", attack);
+        
+
+        
 
         if (moveHorizontal != 0 || moveVertical != 0)
         {
@@ -42,7 +54,7 @@ public class PlayerScript : MonoBehaviour {
         }
         myAnimator.SetFloat("speed", walkSpeed);
 
-        if (moveHorizontal != 0 || moveVertical != 0)
+        if (moveHorizontal != 0 )
         {
             if (moveHorizontal > 0.5)
             {
